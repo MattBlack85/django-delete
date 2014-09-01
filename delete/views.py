76ef80f1cec3fd1de310906b36d927b31c9adffe
MattBlack85 <promat85@gmail.com>
2014-09-01 23:06:25 +0200

    Exempt view from csrf protection for testing purpose

--- delete/views.py
+++ delete/views.py
@@ -1,7 +1,10 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 
+from delete.models import Car
 
+
+@csrf_exempt
 def delete(request, pk):
     if request.method == 'DELETE':
         Car.objects.get(pk=pk).delete()
