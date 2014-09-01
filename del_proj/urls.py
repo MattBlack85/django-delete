a99ce9c1443b9ccf2f4672dbdbf44d523fc55a67
MattBlack85 <promat85@gmail.com>
2014-09-01 22:13:24 +0200

    Add url for deletion

--- del_proj/urls.py
+++ del_proj/urls.py
@@ -4,9 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'del_proj.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+                       url(r'^$', 'del_proj.views.home', name='home'),
+                       url(r'^delete/(?P<pk>\d+)$', include('delete.urls')),
+                       url(r'^admin/', include(admin.site.urls)),
 )
