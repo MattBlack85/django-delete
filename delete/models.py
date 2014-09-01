28b5d7d68906c5a694dab92421266f5f3bf8cdfa
MattBlack85 <promat85@gmail.com>
2014-09-01 22:11:39 +0200

    Create model

--- delete/models.py
+++ delete/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    name = models.CharField(max_length=30)
+    model = models.CharField(max_length=30)
+    color = models.CharField(max_length=30)
